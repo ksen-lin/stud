20935a8e26fd5ddcac50822065f8916ebfaefc53
ksen-lin <37420872+ksen-lin@users.noreply.github.com>
2020-11-22 01:43:17 +0300

    added writing to slack spaces

--- src/fatparse.c
+++ src/fatparse.c
@@ -76,6 +76,7 @@ struct file_record {
     int   fsize;
 };
 
+
 // fat16
 // 0000: free,
 // 0002-ffef: cluster in use; the value given is the number of the next cluster in the file,
@@ -90,40 +91,77 @@ struct options {
     char verbose;
 };
 
+struct slack_space {
+    int offset;
+    short length;
+};
+
+
 /* * * * * * * * * * * * * *
  *      G L O B A L S      *
  * * * * * * * * * * * * * */
+struct slack_space slack[10];
 struct options opts = {0};
 struct fat12bs bootsect;
+FILE  *f = (void *)0;
 short *fat = (void *)0;
 int    fat_n = 0;
+short   slack_n = 0;
+
 
 
 void print_lfn(struct long_filename *addr)
 {
     int i;
-    for (i = 0; i < 5; i += 1) {
+    for (i = 0; i < 5; i += 1)
         putchar(addr->name1[i]);
-    }
-//         putchar(((struct long_filename *)(&root_dir[i]))->name1[i]);
-    for (i = 0; i < 6; i += 1) {
+    for (i = 0; i < 6; i += 1)
         putchar(addr->name2[i]);
-    }
-//         putchar(((struct long_filename *)(&root_dir[i]))->name2[i]);
-    for (i = 0; i < 2; i += 1) {
+    for (i = 0; i < 2; i += 1)
         putchar(addr->name3[i]);
-    }
-//         putchar(((struct long_filename *)(&root_dir[i]))->name3[i]);
-//                 putchar(root_dir[i].lfn.name3[i]);
     return;
 }
 
 
 
-// int inline fat_next_rec(rec)
-// {
-//     return fat[];
-// }
+void show_slacks()
+{
+    int i;
+    printf(" [*] %i SLACK SPACES FOUND:\n", slack_n);
+    for (i = 0; i < slack_n; i += 1) {
+        printf(" %2i: offset  %#-8x\n"
+               "     length  %#4hx (%3$hu)\n", i, slack[i].offset, slack[i].length);
+    }
+}
+
+
+void write2slacks()
+{
+    int choice, off;
+    char *buf;
+    printf("choose space to write to [0-%i]: ", slack_n - 1);
+    scanf("%d%*c", &choice);
+    if (choice >= slack_n) {
+        printf("wrong choice...\n");
+        return;
+    }
+
+    freopen((char*)0, "r+", f);
+    perror("freopen");
+    off = slack[choice].offset + bootsect.bpb.sector_size *
+             bootsect.bpb.sectincluster - slack[choice].length;
+    fseek(f, off, 0);
+    buf = malloc(slack[choice].length);
+
+    printf("enter your data: ");
+    fgets(buf, slack[choice].length, stdin);
+     /* removing LF */
+    buf[strlen(buf) - 1] = '\0';
+    printf("writing `%s' to %#x...\n\n", buf, off);
+    fwrite(buf, 1, strlen(buf), f);
+    perror("fwrite");
+    free(buf);
+}
 
 
 
@@ -142,11 +180,11 @@ short last_clus(short clus)
 
 void show_clus_chain(short clus)
 {
-    if (!clus)
+    if (!opts.verbose || !clus)
         return;
 
     printf("   clusters:\n");
-    while (clus != (short)0xffff ) {
+    while (clus != (short)0xffff && clus != 0) {
         printf("%#x -> ", clus);
         clus = fat[clus];
     }
@@ -167,23 +205,88 @@ void print_file(struct file_record *frec,
     printf("  |  start:  offset %#8x  sector %#x (clus %#x)\n",
            base_sect*cluster_size/sec_per_clus, base_sect, clus_no);
 
-    if (opts.verbose)
-        show_clus_chain(clus_no);
+    show_clus_chain(clus_no);
 
     if (frec->fsize % cluster_size) {
         tmp = last_clus(clus_no);
+        slack[slack_n].offset = cluster_size/sec_per_clus*((tmp-2)*sec_per_clus + fst_data_sect);
+        slack[slack_n].length = cluster_size - frec->fsize % cluster_size;
         printf(" ~~~ %u bytes available in the trailing block ~~~\n"
-               "   you can find it in cluster %#x (offset %#x)\n",
-                cluster_size - frec->fsize % cluster_size, tmp,
-                cluster_size/sec_per_clus*((tmp-2)*sec_per_clus + fst_data_sect));
+               "   you can find it in cluster %#x (offset %#x)\n\n",
+                slack[slack_n].length, tmp, slack[slack_n].offset);
+        slack_n += 1;
     }
 }
 
-void parse_dir(struct file_record *dir)
+#define ENT_NUM bootsect.bpb.sector_size/sizeof(struct file_record)
+/* assert: dir takes only one cluster */
+void parse_dir(struct file_record *dir, int fst_data_sect, int n)
 {
+    if (n++ > 5)
+        return;
+    // TODO: `tabs' to show recursion depth
+    char sp[20];
+    struct file_record entries[ENT_NUM];
+    int i, clus_no   = ((dir->fst_clus_hi << 16) | dir->fst_clus_lo);
+//     memset(sp, ' ', 4*n);
+    sp[0] = '\0';
 
+    printf(" [>] ENTERING LEVEL %i\n", n);
+    show_clus_chain(clus_no);
+    i = (bootsect.bpb.sectincluster * (clus_no-2) + fst_data_sect) *
+           bootsect.bpb.sector_size;
+
+    printf(" [*] seeking to %#x...\n", i);
+    fseek(f, i, 0);
+    fread(entries, 1, sizeof(entries), f);
+
+    // first 2 entries are self and root, so skip them
+    for (i = 2; i < 8; i += 1) {
+        if ((0xff & entries[i].name[0]) == 0xe5 ||
+            (0xff & entries[i].name[0]) == 0x05)
+        {
+            printf("%s [*] record marked deleted\n", sp);
+            continue;
+        } else if ((0xff & entries[i].name[0]) == 0) {
+            printf("%s [*] record and the rest of directory marked deleted\n", sp);
+        } else {
+            printf("%s [+] record ok\n", sp);
+        }
+        printf("%s  |  fst_clus: %#x\n", sp, (entries[i].fst_clus_lo));
+
+        switch (entries[i].attr) {
+        case (ATTR_VOLUME_ID | ATTR_ARCHIVE):
+            printf("%s  |  found volume label record\n", sp);
+            printf("%s  |  name: \"%.11s\"\n", sp, entries[i].name);
+            break;
+
+        case (ATTR_RO | ATTR_HIDDEN | ATTR_SYSTEM | ATTR_VOLUME_ID):
+            if (opts.verbose) {
+                printf("%s  |  lfn (ord %#x)\n  |  \"", sp,
+                       0xff&((struct long_filename *)(&entries[i]))->LDIR_ord);
+                print_lfn((struct long_filename *)&entries[i]);
+                printf("\"\n");
+            }
+            break;
+
+        case ATTR_DIRECTORY:
+            printf("%s  |  directory\n", sp);
+            printf("%s  |  name: \"%.11s\"\n", sp, entries[i].name);
+            parse_dir(&entries[i], fst_data_sect, n);
+            break;
+
+        default:
+            print_file(&entries[i],
+                       bootsect.bpb.sector_size*bootsect.bpb.sectincluster,
+                       bootsect.bpb.sectincluster,
+                       fst_data_sect);
+            break;
+        }
+    }
+    printf("\n [<] LEAVING LEVEL %i\n", n);
     return;
 }
+
 
 int main(int argc, char *argv[])
 {
@@ -194,7 +297,7 @@ int main(int argc, char *argv[])
     int i, tmp;
     int first_data_sector, root_dir_clus, root_dir_sectors;
 
-    FILE *f = fopen("adams.dd", "r");
+    f = fopen("adams.dd", "r");
     perror("fopen");
 
     /* reading FAT16 boot sector */
@@ -237,12 +340,13 @@ int main(int argc, char *argv[])
 
     /* Набор LFN-записей каталога FAT всегда должен быть
      * связан с обычной SFN-записью                      */
-    for (i = 0; i < 16 /*bootsect.bpb.root_fd_num/2*/; i += 1) {
+    for (i = 0; i < 20 /*bootsect.bpb.root_fd_num/2*/; i += 1) {
         putchar('\n');
         if ((0xff & root_dir[i].name[0]) == 0xe5 ||
             (0xff & root_dir[i].name[0]) == 0x05)
         {
             printf(" [*] record marked deleted\n");
+            continue;
         } else if ((0xff & root_dir[i].name[0]) == 0) {
             printf(" [*] record and the rest of directory marked deleted\n");
         } else {
@@ -258,7 +362,8 @@ int main(int argc, char *argv[])
 
         case (ATTR_RO | ATTR_HIDDEN | ATTR_SYSTEM | ATTR_VOLUME_ID):
             if (opts.verbose) {
-                printf("  |  lfn (ord %#x)\n   | \"", 0xff&((struct long_filename *)(&root_dir[i]))->LDIR_ord);
+                printf("  |  lfn (ord %#x)\n  |  \"",
+                       0xff&((struct long_filename *)(&root_dir[i]))->LDIR_ord);
                 print_lfn((struct long_filename *)&root_dir[i]);
                 printf("\"\n");
             }
@@ -267,7 +372,7 @@ int main(int argc, char *argv[])
         case ATTR_DIRECTORY:
             printf("  |  directory\n");
             printf("  |  name: \"%.11s\"\n", root_dir[i].name);
-            parse_dir(&root_dir[i]);
+            parse_dir(&root_dir[i], first_data_sector, 0);
             break;
 
         default:
@@ -278,6 +383,9 @@ int main(int argc, char *argv[])
             break;
         }
     }
+
+    show_slacks();
+    write2slacks();
 
     errno = 0;
     free(fat);
